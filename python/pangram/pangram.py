edf7c8c236d2045a5169beb01405f147431ba5c7
Unknown <otheldobel@autistici.org>
2019-04-29 01:33:11 +0200

    python pangram

---
+++ python/pangram/pangram.py
@@ -0,0 +1,25 @@
+
+
+def is_pangram(sentence):
+    alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
+    sentence_lowered = str.lower(sentence)
+    letters = ""
+    for char in sentence_lowered:
+        if char in alphabet_lower:
+            letters += char
+    for char in alphabet_lower:
+        if char not in sentence_lowered:
+            return False
+
+    return True
+
+"""Better solution
+
+from string import ascii_lowercase
+
+ALPHABET = set(ascii_lowercase)
+
+def is_pangram(string):
+    return ALPHABET.issubset(string.lower())
+
+"""
