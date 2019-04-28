10eaea88b6a453998d185bd860ac5792a11b83dc
Unknown <otheldobel@autistici.org>
2019-04-29 01:37:32 +0200

    python pangram autre solution

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -23,3 +23,17 @@ def is_pangram(string):
     return ALPHABET.issubset(string.lower())
 
 """
+
+"""
+from string import ascii_lowercase
+
+
+def is_pangram(sentence):
+    sentence_set = set(sentence.lower())
+    alpha_set = set(ascii_lowercase)
+
+    if alpha_set - sentence_set == set([]):
+        return True
+
+    return False
+"""
