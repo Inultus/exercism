294be518f74222f0dedc05bc8f7c300c75bad049
Unknown <otheldobel@autistici.org>
2019-05-11 18:21:43 +0200

    fsharp leap better solution

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,12 +1,19 @@
 ﻿module Leap
 
+let leapYear (year:int):bool =
+     year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0)
+
+// ==============================================
+(*
   let leapYear (year:int):bool =
      match year with
      | year when year % 4 <> 0 -> false
      | year when year % 100 <> 0 -> true
      | year when year % 400 <> 0   -> false
      | _ -> true
+*)
 
+// ==============================================
 // let (|DivBy|_|) b a =
 //     if a % b = 0 then Some(DivBy)
 //     else None
@@ -17,6 +24,7 @@
 //     | DivBy 100 -> false
 //     | DivBy 4 -> true
 //     | _ -> false
+// ==============================================
 
 // let isLeapYear x =
 //     match (x % 4, x % 100, x % 400) with
