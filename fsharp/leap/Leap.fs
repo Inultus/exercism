846351564401030276ff48c401daa39e33a590cc
Unknown <otheldobel@autistici.org>
2019-05-14 22:48:44 +0200

    fsharp leap better writing

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,34 +1,8 @@
 ﻿module Leap
 
+let isDivisibleBy divisor dividend = 
+     dividend % divisor = 0
+
 let leapYear (year:int):bool =
-     year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0)
+     isDivisibleBy 4 year && not(isDivisibleBy 100 year) || isDivisibleBy 400 year
 
-// ==============================================
-(*
-  let leapYear (year:int):bool =
-     match year with
-     | year when year % 4 <> 0 -> false
-     | year when year % 100 <> 0 -> true
-     | year when year % 400 <> 0   -> false
-     | _ -> true
-*)
-
-// ==============================================
-// let (|DivBy|_|) b a =
-//     if a % b = 0 then Some(DivBy)
-//     else None
-
-// let isLeapYear year =
-//     match year with
-//     | DivBy 400 -> true
-//     | DivBy 100 -> false
-//     | DivBy 4 -> true
-//     | _ -> false
-// ==============================================
-
-// let isLeapYear x =
-//     match (x % 4, x % 100, x % 400) with
-//     | (_, _, 0) -> true
-//     | (_, 0, _) -> false
-//     | (0, _, _) -> true
-//     | _ -> false
